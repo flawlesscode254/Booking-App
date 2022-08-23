016ef04a2d85b1a2421ea9aa2cdbabce53b72b2b
flawlesscode254 <denzme414@gmail.com>
2022-08-23 11:10:35 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	name := "Duncan"
+	fmt.Println(name)
+
+	var email = "duncanii414@gmail.com"
+	fmt.Println(email)
+
+	num := 4
+	fmt.Println(num)
+
+	var count = 7
+	fmt.Println(count)
+
+	var username int = 34
+	fmt.Println(username)
+
+	var state bool = false
+	fmt.Println(state)
+
+	//var identity string
+	//
+	//fmt.Println("What is your name:")
+	//fmt.Scan(&identity)
+	//
+	//fmt.Printf("Your username is: %v", identity)
+
+	// Sum calculations
+	a := 12
+	b := 13
+
+	// Operations
+	fmt.Println(a - b)
+	fmt.Println(a + b)
+	fmt.Println(a * b)
+	fmt.Println(a / b)
+	fmt.Println(a % b)
+
+	// Arrays implementation in Go
+	
+}
